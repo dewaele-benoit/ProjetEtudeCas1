2fcdbe90096bcf4dce82c0637b3f01f751846714
dewaele-benoit <benoit.dewaele@hotmail.com>
2017-11-02 16:02:26 +0100

    Ajout attribut + constructeur + getter +setter

--- Library/src/com/benoit/epsi/library/Book.java
+++ Library/src/com/benoit/epsi/library/Book.java
@@ -1,5 +1,50 @@
 package com.benoit.epsi.library;
 
 public class Book {
+	
+	private String id;
+	private String author;
+	private String title;
+	private String ISBN;
+	
+	public Book (String unId, String unAuthor, String unTitle, String unISBN){
+		this.id=unId;
+		this.author=unAuthor;
+		this.title=unTitle;
+		this.ISBN=unISBN;
+	}
 
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getISBN() {
+		return ISBN;
+	}
+
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+	
+	
 }
--- Library/src/com/benoit/epsi/library/Client.java
+++ Library/src/com/benoit/epsi/library/Client.java
@@ -1,5 +1,21 @@
 package com.benoit.epsi.library;
 
 public class Client {
+	
+	private String username;
+	
+	public Client(String unUsername){
+		this.username=unUsername;
+	}
 
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	
+	
 }
